c69e318dd3f9578e6cbe92d60c45b2126ba35638
Paul Gross <p.gross6408@gmail.com>
2024-08-16 19:02:13 +0200

    function reviews

--- script/like.js
+++ script/like.js
@@ -1,16 +1,16 @@
 // Der Code wird ausgeführt, sobald das gesamte HTML-Dokument geladen ist.
 document.addEventListener(
   "DOMContentLoaded", // Dieses Event tritt ein, wenn die gesamte HTML-Seite geladen ist.
-  function () { 
+  function () {
     // Diese Funktion wird ausgeführt, wenn die Seite vollständig geladen ist.
-    
-    const img = document.getElementById("like-icon"); 
+
+    const img = document.getElementById("like-icon");
     // Speichert das HTML-Element mit der ID "like-icon" in der Variablen 'img'.
 
     if (!localStorage.getItem("icon")) {
       // Prüft, ob im lokalen Speicher des Browsers ("localStorage") ein Eintrag namens "icon" existiert.
       // Wenn kein Eintrag existiert (d.h. der Benutzer hat noch nie auf den Like-Button geklickt), wird der Code in diesem Block ausgeführt.
-      
+
       img.src = "./assets/icons/like-false.svg";
       // Setzt das Bild der "like-icon" auf das "Nicht-geliked"-Icon (like-false.svg).
     } else {
@@ -29,7 +29,7 @@ function toggleLike() {
 
   if (img.src.includes("like-false.svg")) {
     // Prüft, ob die aktuelle Bildquelle (img.src) das "Nicht-geliked"-Icon enthält.
-    
+
     img.src = "./assets/icons/like-true.svg";
     // Wenn das Bild aktuell das "Nicht-geliked"-Icon ist, ändert es die Quelle auf das "Geliked"-Icon (like-true.svg).
 
